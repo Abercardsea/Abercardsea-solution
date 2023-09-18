2d532d37c0a3f808df23e34d4a38a61d188e8289
Rhys Shaw <rhysc3p0@gmail.com>
2023-09-18 17:14:39 +0100

    updated front-end server

--- server/view.py
+++ server/view.py
@@ -1,22 +1,27 @@
-from flask import Blueprint, render_template, request, flash, jsonify
-
+from flask import Blueprint, render_template, request
 from . import db
-import json
-import os
 from sqlalchemy import text
-from flask import Flask, render_template
-from sqlalchemy import create_engine, text
-from sqlalchemy.orm import sessionmaker
-from datetime import datetime
-import matplotlib.pyplot as plt
+from flask import render_template
+from sqlalchemy import text
 import pandas as pd
-from io import StringIO, BytesIO
-import base64
+from flask_navigation import Navigation
 
 views = Blueprint('views', __name__)
 
+# we only have one access point. The home route e.g. url will be example.com/
+# you could have alternative routes that have different urls e.g. example.com/about
+
+
 @views.route('/', methods=['GET', 'POST'])
 def home():
+    '''
+
+    Here we are getting the data from the database and sending it to the frontend server.
+    This funciton allso renders the home.html page and sends it to the user.
+
+    return: home.html
+
+    '''
     # get data from the database
     Weather_query = text("""
     SELECT DISTINCT * FROM  met_office
@@ -77,34 +82,67 @@ def home():
     )
     """)
 
+    Imbalance_market_query = text("""
+    SELECT DISTINCT *  FROM  bmreport
+    WHERE DATE(SettlementDateTime) = (
+        SELECT DATE(MAX(SettlementDateTime),'-1 day') FROM bmreport
+    )
+    """)
 
-    data_wind = pd.DataFrame(db.session.execute(Windpred_query).fetchall())
-    data_solar = pd.DataFrame(db.session.execute(Solarpred_query).fetchall())
-    data_weather = pd.DataFrame(db.session.execute(Weather_query).fetchall())
-    data_onsite_demand = pd.DataFrame(db.session.execute(onsite_query).fetchall())
-    data_EnergyUsage_query = pd.DataFrame(db.session.execute(EnergyUsage_query).fetchall()).sort_values(by=['Settlement Period'])
-    data_EnergyType_query = pd.DataFrame(db.session.execute(EnergyType_query).fetchall()).sort_values(by=['Settlement Period'])
-    data_Predicted_price_query = pd.DataFrame(db.session.execute(Predicted_price_query).fetchall())
+    try:
+        data_wind = pd.DataFrame(db.session.execute(Windpred_query).fetchall())
+        data_solar = pd.DataFrame(db.session.execute(Solarpred_query).fetchall())
+        data_weather = pd.DataFrame(db.session.execute(Weather_query).fetchall())
+        data_onsite_demand = pd.DataFrame(db.session.execute(onsite_query).fetchall())
+        data_EnergyUsage_query = pd.DataFrame(db.session.execute(EnergyUsage_query).fetchall()).sort_values(by=['Settlement Period'])
+        data_EnergyType_query = pd.DataFrame(db.session.execute(EnergyType_query).fetchall()).sort_values(by=['Settlement Period'])
+        data_Predicted_price_query = pd.DataFrame(db.session.execute(Predicted_price_query).fetchall())
 
-    DateTime= data_weather.DateTime.to_list()
-    Temperature = data_weather['T'].to_list()
-    data_windspeed = data_wind['WindSpeed'].to_list()
-    data_windpower = data_wind['WindPower'].to_list()
-    data_windDateTime = data_wind['time'].to_list()
-    data_solarPower = data_solar['SolarPower'].to_list()
-    data_solarDateTime = data_solar['time'].to_list()
-    data_EnergyUsage = data_EnergyUsage_query['Quantity'].to_list()
-    data_EnergyUsageDateTime = data_EnergyUsage_query['Settlement Date'].to_list()[0]
-    data_EnergyUsagePeriods = data_EnergyUsage_query['Settlement Period'].to_list()
+        DateTime= data_weather.DateTime.to_list()
+        Temperature = data_weather['T'].to_list()
+        data_windspeed = data_wind['WindSpeed'].to_list()
+        data_windpower = data_wind['WindPower'].to_list()
+        data_windDateTime = data_wind['time'].to_list()
+        data_solarPower = data_solar['SolarPower'].to_list()
+        data_solarDateTime = data_solar['time'].to_list()
+        data_EnergyUsage = data_EnergyUsage_query['Quantity'].to_list()
+        data_EnergyUsageDateTime = data_EnergyUsage_query['Settlement Date'].to_list()[0]
+        data_EnergyUsagePeriods = data_EnergyUsage_query['Settlement Period'].to_list()
 
-    PricePredicted = data_Predicted_price_query['price'].to_list()
-    PricePredictedDateTime = data_Predicted_price_query['time'].to_list()
+        PricePredicted = data_Predicted_price_query['price'].to_list()
+        PricePredictedDateTime = data_Predicted_price_query['time'].to_list()
+
+        Imbalance_Market_query = pd.DataFrame(db.session.execute(Imbalance_market_query).fetchall())
+        Imbalance_Market = Imbalance_Market_query['ImbalancePriceAmount'].to_list()
+        Imbalance_Market_Datetime = Imbalance_Market_query['SettlementDateTime'].to_list()
+
+        status = 1
+
+    except:
+        print('error getting db data!! Is the database configured?')
+        # all empty lists
+        Temperature = []
+        DateTime = []
+        data_windspeed = []
+        data_windpower = []
+        data_windDateTime = []
+        data_solarPower = []
+        data_solarDateTime = []
+        data_EnergyUsage = []
+        data_EnergyUsageDateTime = []
+        data_EnergyUsagePeriods = []
+        PricePredicted = []
+        PricePredictedDateTime = []
+        Imbalance_Market = []
+        Imbalance_Market_Datetime = []
+        status = 0
 
     # Date from a DateTime object
     #print(DateTime[0])
     #date = datetime.strptime(DateTime[0],"%Y-%m-%dT%H:%M:%SZ").date()
     #print('DATE: ',date)
 
+    # we are parseing the date from database to the fontend server.
 
     return render_template("home.html", temps=Temperature, dates=DateTime,
                            windspeed=data_windspeed, windpower=data_windpower,
@@ -112,4 +150,9 @@ def home():
                            solarDateTime=data_solarDateTime, onsite_Datetime=data_onsite_demand['DateTime'].to_list(),
                            onsite_Total_demand=data_onsite_demand['Total demand'].to_list(),EnergyUsage=data_EnergyUsage,
                            EnergyUsageDateTime=data_EnergyUsageDateTime,EnergyUsagePeriods=data_EnergyUsagePeriods,
-                           PricePredicted=PricePredicted,PricePredictedDateTime=PricePredictedDateTime)
+                           PricePredicted=PricePredicted,PricePredictedDateTime=PricePredictedDateTime,status=status,
+                           Imbalance_Market=Imbalance_Market,Imbalance_Market_Datetime=Imbalance_Market_Datetime)
+
+@views.route('/doc', methods=['GET', 'POST'])
+def docs():
+    return render_template("docs.html")
